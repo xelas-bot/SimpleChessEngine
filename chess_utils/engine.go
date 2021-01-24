475a87e7f2ac196ff9d8ff5f7fedfd3f1bab41d6
dlz9345 <dlz9345@gmail.com>
2021-01-24 14:59:54 -0600

    Engine

--- chess_utils/engine.go
+++ chess_utils/engine.go
@@ -24,23 +24,23 @@ func negamax(board *Board, depth int, a int, b int, player bool) int {
 		} else {
 			return Evaluate(board)
 		}
-
-		MoveList := GetBoardMoves(board)
-
-		value := int(math.Inf(-1))
-		for _, move := range MoveList {
-
-			eval := negamax(board, depth-1, -b, -a, !player)
-			if eval > value {
-				value = eval
-			}
-			if value > a {
-				a = value
-			}
-			if a >= b {
-				break
-			}
-		}
-		return value
 	}
+
+	MoveList := GetBoardMoves(board)
+
+	value := int(math.Inf(-1))
+	for _, move := range MoveList {
+
+		eval := negamax(board, depth-1, -b, -a, !player)
+		if eval > value {
+			value = eval
+		}
+		if value > a {
+			a = value
+		}
+		if a >= b {
+			break
+		}
+	}
+	return value
 }
